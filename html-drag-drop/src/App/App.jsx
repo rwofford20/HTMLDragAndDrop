d24978f26b6469665437ac71dd43a3c757be3e74
Jude Battista <jbattista20@my.whitworth.edu>
2020-04-14 14:13:22 -0700

    Add drag functionality to ToDoDragDropDemo component

--- html-drag-drop/src/App/App.jsx
+++ html-drag-drop/src/App/App.jsx
@@ -11,10 +11,59 @@ export default class ToDoDragDropDemo extends Component {
         ]
     }
     
+    onDragStart = (event, taskName) => {
+        console.log('drag start on div: '. taskName);
+        event.dataTransfer.setData("taskName", taskName);
+    }
+
+    onDragOver = (event) => {
+        event.preventDefault();
+    }
+
+    onDrop = (event, cat) => {
+        let taskName = event.dataTransfer.getData("taskName");
+        let tasks = this.state.tasks.filter((task) => {
+            if (task.taskName == taskName) {
+                task.type = cat;
+            }
+            return task;
+        });
+
+        this.setState({
+            ...this.state,
+            tasks
+        });
+    }
+
     render() {
+        var tasks = {
+            inProgress: [],
+            Done: []
+        }
+
+        this.state.tasks.foreach((task) => {
+            tasks[task.type].push(
+                <div key={task.id}
+                    onDragStart={(event) => this.onDragStart(event, task.taskName)}
+                    draggable
+                    className="draggable"
+                    style={{backgroundColor: task.backgroundColor}}> {/* may need to be task.bgColor */}
+                        {task.taskName}
+                    </div>   
+            );
+        });
+
         return (
             <div className="drag-container">
                 <h2 className="head">To Do List Drag & Drop</h2>
+                <div className="inProgress"
+                    onDragOver={(event) => this.onDragOver(event)}
+                    onDrop={(event) => {this.onDrop(event, "inProgress")}}
+                >
+                    <span className="group-header">
+                        In Progress
+                    </span>
+                </div>
             </div>
         );
     }
